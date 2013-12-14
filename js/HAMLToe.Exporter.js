88da2eed6884d86fc464a4e66afbca53c5880e0b
Rob Johnston <rob@rjohnst.com>
2013-12-14 23:57:31 +1000

    pulled the exporter out to be its own file/class/thingy

---
+++ js/HAMLToe.Exporter.js
@@ -0,0 +1,17 @@
+(function () {
+
+    HAMLToe.Exporter = function() {
+        var exporter = document.getElementById("hamltoe-export");
+
+        exporter.onclick = function() {
+            this.herf = "";
+            // TODO test for a file name to use, let the user know if there's not one set and return
+            this.href = "data:application/octet-stream,".concat(encodeHaml());
+            this.download = "wakka.txt";
+        };
+
+        function encodeHaml() {
+            return encodeURI(document.getElementById("hamltoe-input").value);
+        };
+    };
+})();
